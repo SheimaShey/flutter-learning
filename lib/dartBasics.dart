25f963c1e107df7e40d15b7567cfbfda56dd1692
“Şeyma <seymabuyukaydin7@gmail.com>
2026-06-27 15:24:13 +0300

    gun1:dart basics

---
+++ lib/dartBasics.dart
@@ -0,0 +1,37 @@
+void main() {
+  var name = "Şeyma";
+  name= "luigi";
+  print(name);
+  
+  final age = 25;
+  print(age);
+  
+  const age1 =21;
+  print(age1);
+  
+  print(age + 10);
+  print(age - 10 );
+  print(age * 10 );
+  print(age % 10 );
+  print(age / 10 );
+  
+  print("My name is " + name);
+  print("My name is $name and my age is $age");
+  
+  String name1 = "mario";
+  print(name1);
+  
+  bool isOpen = true;
+  isOpen= false;
+  print(isOpen);
+  
+  double averageRating = 7;
+  averageRating = 7.9;
+  print(averageRating);
+  
+  int? points;
+  print(points);  // Output = null
+  
+  // Dart basics
+  
+}
